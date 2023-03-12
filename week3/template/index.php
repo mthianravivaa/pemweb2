fb09b4ec69eb1220f2428649f1f5a1d1e72bf49e
mthianravivaa <114566512+mthianravivaa@users.noreply.github.com>
2023-03-12 21:20:33 +0700

    Add files via upload

---
+++ week3/template/index.php
@@ -0,0 +1,14 @@
+<!-- Panggil File atas.php -->
+<?php
+require_once('atas.php')
+?>
+
+<!-- Panggil File content.php -->
+<?php
+require_once('content.php')
+?>
+
+<!-- Panggil File bawah.php -->
+<?php
+require_once('bawah.php')
+?>
