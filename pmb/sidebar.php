1b641fe123cf7b8cacfdea3969782c52822be65d
mthianravivaa <mthianuraviva@gmail.com>
2023-03-23 21:54:31 +0700

    upload week4

---
+++ pmb/sidebar.php
@@ -0,0 +1,81 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/nurulfikri.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info">
+          <a href="#" class="d-block">Meuthia Nuraviva</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./array_siswa.php" class="nav-link">
+                  <i class="fas fa-circle nav-icon"></i>
+                  <p>
+                  Praktikum-1
+                  <i class="fas fa-angle-left right"></i>
+                  </p>
+                </a>
+               <li class="nav-item">
+                <a href="index_siswa.php" class="nav-link">
+                  <i class="fas fa-circle nav-icon"></i>
+                  <p>
+                  Praktikum-2
+                  <i class="fas fa-angle-left right"></i>
+                  </p>
+                </a>
+               <li class="nav-item">
+                <a href="form_register.php" class="nav-link">
+                  <i class="fas fa-circle nav-icon"></i>
+                  <p>
+                  Praktikum-3
+                  <i class="fas fa-angle-left right"></i>
+                  </p>
+                </a>
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Logout
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
